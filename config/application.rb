dce18cc87b08c620a129ef4756bed2676345b048
wagurano <wagurano@gmail.com>
2015-06-08 21:34:49 +0900

    remove secretes to local_env.yml

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,11 @@ module Mers
 
     config.autoload_paths += %W("#{config.root}/app/crawler/*/")
     config.autoload_paths += %W("#{config.root}/app/constants/*/")
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
